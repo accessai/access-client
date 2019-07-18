633cb0a4dab6f4a4b66aa39040044c623ff1697b
Majeed <majeedk526@gmail.com>
2019-07-18 19:04:54 +0530

    minor fix

--- access_client/about.py
+++ access_client/about.py
@@ -4,6 +4,6 @@ __version__ = "0.0.1"
 __summary__ = "Client for accessai solutions"
 __uri__ = "http://accessai.co"
 __author__ = "ConvexHull Technology"
-__email__ = "contact@accessai.co"
+__email__ = "support@accessai.co"
 __license__ = "Apache 2.0"
 __release__ = True
